ed87099b3aa0fbcd4ff9a213a4d518456d89e5c9
Eidrian2005 <eserrano@fwdcostarica.com>
2024-09-24 16:04:45 -0600

    faltan agregar el buscador en admin task y tambien darle estilos a la pagina

--- proyect-react/src/App.jsx
+++ proyect-react/src/App.jsx
@@ -8,6 +8,7 @@ import 'react-toastify/dist/ReactToastify.css';
 function App() {
   
   const [count, setCount] = useState(0)
+  
 
   return (  
     <>
--- proyect-react/src/components/Header.jsx
+++ proyect-react/src/components/Header.jsx
@@ -6,16 +6,12 @@ import imgLogo from '../img/Logo-sala-de-belleza.jpg'
 
 function Header() {
   return (
-    <header>
+    <header >
       <div className="headermiddle">
         <div className="middleContainer">
           <img src={imgLogo} alt="Logo" id="logo" />
           <form action="">
-
-            
             <input type="text" name="" id="buscador" />
-
-
           </form>
           <button>Iniciar Sesion</button>
         </div>
